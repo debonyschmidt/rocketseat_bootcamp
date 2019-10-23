7d3babc3ed288e748847ac5bad4ec1279239e3a4
Debony SCHMIDT <debony_13_@hotmail.com>
2019-10-23 21:13:04 +1100

    Gobarber2

--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import user from './app/models/user';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Australia' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Debony Schmidt',
+    email: 'debonyschmidt@gmail.com',
+    password_hash: '123456'
+  });
+
+  return res.json(user);
 });
 
 export default routes;
