8a2445ee641260f3c1b45f70cdb7506d217de1e5
Debony SCHMIDT <debony_13_@hotmail.com>
2019-11-04 20:23:12 +1100

    Created POST users

--- GoBarber/src/routes.js
+++ GoBarber/src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Debony Schmidt',
-    email: 'debony_13_@hotmail.com',
-    password_hash: '1234556',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
