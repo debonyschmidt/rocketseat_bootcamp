7d4a9f60eaca378622a8b2c938553da0838b289b
Debony SCHMIDT <debony_13_@hotmail.com>
2019-11-07 12:14:03 +1100

    New route PUT

--- GoBarber/src/app/controllers/UserController.js
+++ GoBarber/src/app/controllers/UserController.js
@@ -3,11 +3,7 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
-    const schema = Yup.object().shape({
-      name: Yup.string().required().
-    });
-
-    const userExists = await User.findOne({ where: { email: req.body.email }});
+   const userExists = await User.findOne({ where: { email: req.body.email }});
 
     if (userExists) {
       return res.status(400).json({ error: 'User already exists' });
